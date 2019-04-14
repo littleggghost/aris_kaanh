83c61e295e08db1099cd94b76bc624034997ae9a
KevinLiao888 <Lnch-628120@163.com>
2019-04-14 16:59:36 +0800

    add moveJRT for linx

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,8 @@
 #include<filesystem>
 
 
-std::atomic_bool is_automatic = false;
 using namespace aris::dynamic;
 
-double fce_data[buffer_length], fce_send[buffer_length];
-int data_num = 0, data_num_send = 0;
-std::vector<std::vector<std::string>> plantrack(6, std::vector<std::string>());
-std::atomic_int which_di = 0;
 
 auto xmlpath = std::filesystem::absolute(".");
 const std::string xmlfile = "rokae.xml";
