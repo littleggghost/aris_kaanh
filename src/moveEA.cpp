cbb97c0e7c1597e714417bcfb6a493d0f1f04f1a
KevinLiao888 <lnch-628120@163.com>
2019-09-20 11:46:33 +0800

    two jmc motor

--- src/moveEA.cpp
+++ src/moveEA.cpp
@@ -47,6 +47,44 @@ auto createControllerRokaeXB4()->std::unique_ptr<aris::control::Controller>
         "</EthercatMotion>";
 
     controller->slavePool().add<aris::control::EthercatMotion>().loadXmlStr(xml_str);
+
+    xml_str =
+        "<EthercatMotion phy_id=\"1\" product_code=\"0x20181302\""
+        " vendor_id=\"0x66668888\" revision_num=\"0x20181011\" dc_assign_activate=\"0x0300\""
+        " min_pos=\"-536870\" max_pos=\"536870\" max_vel=\"6.28\" min_vel=\"-6.28\""
+        " max_acc=\"31.4\" min_acc=\"-31.4\" max_pos_following_error=\"0.1\" max_vel_following_error=\"0.01\""
+        " home_pos=\"0\" pos_factor=\"4000\" pos_offset=\"0.0\">"
+        "	<SyncManagerPoolObject>"
+        "       <SyncManager is_tx=\"false\"/>"
+        "       <SyncManager is_tx=\"true\"/>"
+        "       <SyncManager is_tx=\"false\">"
+        "           <Pdo index=\"0x1600\" is_tx=\"false\">"
+        "               <PdoEntry name=\"entry\" index=\"0x6040\" subindex=\"0x00\" size=\"16\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x6060\" subindex=\"0x00\" size=\"8\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x607a\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60b8\" subindex=\"0x00\" size=\"16\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60fe\" subindex=\"0x01\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60fe\" subindex=\"0x02\" size=\"32\"/>"
+        "           </Pdo>"
+        "       </SyncManager>"
+        "       <SyncManager is_tx=\"true\">"
+        "           <Pdo index=\"0x1a00\" is_tx=\"true\">"
+        "               <PdoEntry name=\"entry\" index=\"0x603f\" subindex=\"0x00\" size=\"16\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x6041\" subindex=\"0x00\" size=\"16\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x6061\" subindex=\"0x00\" size=\"8\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x6064\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60b9\" subindex=\"0x00\" size=\"16\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60ba\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60bb\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60bc\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60bd\" subindex=\"0x00\" size=\"32\"/>"
+        "               <PdoEntry name=\"entry\" index=\"0x60fd\" subindex=\"0x00\" size=\"32\"/>"
+        "           </Pdo>"
+        "       </SyncManager>"
+        "	</SyncManagerPoolObject>"
+        "</EthercatMotion>";
+
+    controller->slavePool().add<aris::control::EthercatMotion>().loadXmlStr(xml_str);
 	return controller;
 };
 
@@ -109,13 +147,11 @@ auto MoveJS::executeRT(PlanTarget &target)->int
 		// 获取当前起始点位置 //
 		if (target.count == 1)
 		{
-            cout <<"1"<< std::endl;
 			begin_pjs = target.controller->motionPool()[0].actualPos();
 			step_pjs = target.controller->motionPool()[0].actualPos();
 		}
 		step_pjs = begin_pjs + param.j1 * (1 - std::cos(2 * PI*target.count / time)) / 2;
 		target.controller->motionPool().at(0).setTargetPos(step_pjs);
-        cout <<"2"<< std::endl;
 	}
 	else if ((time / 2 < target.count) && (target.count <= totaltime - time / 2))
 	{
@@ -142,22 +178,18 @@ auto MoveJS::executeRT(PlanTarget &target)->int
 	}
 
 
-	// 打印电流 //
+    // 打印电流 //
 	if (target.count % 100 == 0)
 	{
 		cout << "pos"  << ":" << controller->motionAtAbs(0).actualPos() << "  ";
-		cout << "vel"  << ":" << controller->motionAtAbs(0).actualVel() << "  ";
-		cout << "cur"  << ":" << controller->motionAtAbs(0).actualCur() << "  ";
-
 		cout << std::endl;
 	}
 
-	// log 电流 //
+    // log 电流 //
 	auto &lout = controller->lout();
 	lout << controller->motionAtAbs(0).targetPos() << ",";
 	lout << controller->motionAtAbs(0).actualPos() << ",";
-	lout << controller->motionAtAbs(0).actualVel() << ",";
-	lout << controller->motionAtAbs(0).actualCur() << ",";
+
 	lout << std::endl;
 
 	return totaltime - target.count;
@@ -179,112 +211,181 @@ MoveJS::MoveJS(const std::string &name) :Plan(name)
 // moveEAP //
 struct MoveEAPParam
 {
-	double axis_begin_pos;
-	double axis_pos;
-	double axis_vel;
-	double axis_acc;
-	double axis_dec;
-	bool abs;
+    std::vector<double> axis_begin_pos;
+    std::vector<double> axis_pos;
+    std::vector<double> axis_vel;
+    std::vector<double> axis_acc;
+    std::vector<double> axis_dec;
+    bool abs;
 };
 auto MoveEAP::prepairNrt(const std::map<std::string, std::string> &params, PlanTarget &target)->void
 {
     auto c = target.controller;
-	MoveEAPParam param;
-    param.axis_begin_pos = 0.0;
+    MoveEAPParam param;
 
-	for (auto &p : params)
-	{
-		if (p.first == "pos")
-		{
-			param.axis_pos = std::stod(p.second);
-		}
-		else if (p.first == "vel")
-		{	
-			param.axis_vel = std::stod(p.second);
-		}
-		else if (p.first == "acc")
-		{
-			param.axis_acc = std::stod(p.second);
-		}
-		else if (p.first == "dec")
-		{
-			param.axis_dec = std::stod(p.second);
-		}
-		else if (p.first == "ab")
-		{
-			param.abs = std::stoi(p.second);
-		}
-	}
+    const aris::Size num = 2;
+    param.axis_begin_pos.resize(num,0.0);
+    param.axis_pos.resize(num,0.0);
+    param.axis_vel.resize(num,0.0);
+    param.axis_acc.resize(num,0.0);
+    param.axis_dec.resize(num,0.0);
 
-	target.param = param;
-	std::string ret = "ok";
-	target.ret = ret;
+    for (auto &p : params)
+    {
+        if (p.first == "pos")
+        {
+            auto pos = target.model->calculator().calculateExpression(p.second);
+            if(pos.size()==1)
+            {
+                std::fill(param.axis_pos.begin(), param.axis_pos.end(), pos.toDouble());
+            }
+            else if(pos.size() == param.axis_pos.size())
+            {
+                param.axis_pos.assign(pos.begin(), pos.end());
+            }
+            else
+            {
+                throw std::runtime_error(__FILE__+ std::to_string(__LINE__)+"failed");
+            }
+        }
+        else if (p.first == "vel")
+        {
+            auto vel = target.model->calculator().calculateExpression(p.second);
+            if(vel.size()==1)
+            {
+                std::fill(param.axis_vel.begin(), param.axis_vel.end(), vel.toDouble());
+            }
+            else if(vel.size() == param.axis_vel.size())
+            {
+                param.axis_vel.assign(vel.begin(), vel.end());
+            }
+            else
+            {
+                throw std::runtime_error(__FILE__+ std::to_string(__LINE__)+"failed");
+            }
+        }
+        else if (p.first == "acc")
+        {
+            auto acc = target.model->calculator().calculateExpression(p.second);
+            if(acc.size()==1)
+            {
+                std::fill(param.axis_acc.begin(), param.axis_acc.end(), acc.toDouble());
+            }
+            else if(acc.size() == param.axis_acc.size())
+            {
+                param.axis_acc.assign(acc.begin(), acc.end());
+            }
+            else
+            {
+                throw std::runtime_error(__FILE__+ std::to_string(__LINE__)+"failed");
+            }
+        }
+        else if (p.first == "dec")
+        {
+            auto dec = target.model->calculator().calculateExpression(p.second);
+            if(dec.size()==1)
+            {
+                std::fill(param.axis_dec.begin(), param.axis_dec.end(), dec.toDouble());
+            }
+            else if(dec.size() == param.axis_dec.size())
+            {
+                param.axis_dec.assign(dec.begin(), dec.end());
+            }
+            else
+            {
+                throw std::runtime_error(__FILE__+ std::to_string(__LINE__)+"failed");
+            }
+        }
+        else if (p.first == "ab")
+        {
+            param.abs = std::stoi(p.second);
+        }
+    }
+
+    target.param = param;
+    std::vector<std::pair<std::string, std::any>>ret;
+    target.ret = ret;
 
 }
 auto MoveEAP::executeRT(PlanTarget &target)->int
 {
-	auto &param = std::any_cast<MoveEAPParam&>(target.param);
-	// 访问主站 //
+    auto &param = std::any_cast<MoveEAPParam&>(target.param);
+    // 访问主站 //
     auto controller = target.controller;
-	// 第一个count，取各个电机的当前位置
-	if (target.count == 1)
-	{
-		param.axis_begin_pos = controller->motionAtAbs(0).actualPos();
-	}
+    auto &cout = controller->mout();
+    // 第一个count，取各个电机的当前位置
+    if (target.count == 1)
+    {
+        for(aris::Size i=0; i<param.axis_pos.size();i++)
+        {
+            param.axis_begin_pos[i] = controller->motionAtAbs(i).actualPos();
+            cout << "axis_begin_pos:" << param.axis_begin_pos[i] << std::endl;
+        }
+    }
     aris::Size total_count{ 1 };
-	double p, v, a;
+    double p, v, a;
     aris::Size t_count;
-	//绝对轨迹//
-	if (param.abs)
-	{
-		//梯形轨迹规划函数moveAbsolute对输入的量(target.count, param.axis_begin_pos_vec[i], param.axis_pos_vec[i], param.axis_vel_vec[i] / 1000, param.axis_acc_vec[i] / 1000 / 1000, param.axis_dec_vec[i] / 1000 / 1000)都会取绝对值
-		//然后规划出当前count的指令位置p，指令速度v，指令加速度a，以及本梯形轨迹的总count数t_count 
-		aris::plan::moveAbsolute(target.count, param.axis_begin_pos, param.axis_pos, param.axis_vel / 1000, param.axis_acc / 1000 / 1000, param.axis_dec / 1000 / 1000, p, v, a, t_count);
-		controller->motionAtAbs(0).setTargetPos(p);
-		total_count = std::max(total_count, t_count);
-	}
-	//相对轨迹//
-	else
-	{
-		aris::plan::moveAbsolute(target.count, param.axis_begin_pos, param.axis_begin_pos + param.axis_pos, param.axis_vel / 1000, param.axis_acc / 1000 / 1000, param.axis_dec /1000 / 1000, p, v, a, t_count);
-		controller->motionAtAbs(0).setTargetPos(p);
-		total_count = std::max(total_count, t_count);
-	}
+    //绝对轨迹//
+    if (param.abs)
+    {
+        //梯形轨迹规划函数moveAbsolute对输入的量(target.count, param.axis_begin_pos_vec[i], param.axis_pos_vec[i], param.axis_vel_vec[i] / 1000, param.axis_acc_vec[i] / 1000 / 1000, param.axis_dec_vec[i] / 1000 / 1000)都会取绝对值
+        //然后规划出当前count的指令位置p，指令速度v，指令加速度a，以及本梯形轨迹的总count数t_count
+        for(aris::Size i=0; i<param.axis_pos.size();i++)
+        {
+            aris::plan::moveAbsolute(target.count, param.axis_begin_pos[i], param.axis_pos[i], param.axis_vel[i] / 1000, param.axis_acc[i] / 1000 / 1000, param.axis_dec[i] / 1000 / 1000, p, v, a, t_count);
+            controller->motionAtAbs(i).setTargetPos(p);
+            total_count = std::max(total_count, t_count);
+        }
+    }
+    //相对轨迹//
+    else
+    {
+        for(aris::Size i=0; i<param.axis_pos.size();i++)
+        {
+            aris::plan::moveAbsolute(target.count, param.axis_begin_pos[i], param.axis_begin_pos[i]+param.axis_pos[i], param.axis_vel[i] / 1000, param.axis_acc[i] / 1000 / 1000, param.axis_dec[i] / 1000 / 1000, p, v, a, t_count);
+            controller->motionAtAbs(i).setTargetPos(p);
+            total_count = std::max(total_count, t_count);
+        }
+    }
 
-	// 每1000ms打印 目标位置、实际位置、实际速度、实际电流 //
-	auto &cout = controller->mout();
-	if (target.count % 1000 == 0)
-	{
-		cout << controller->motionAtAbs(0).targetPos() << "  " 
-			<< controller->motionAtAbs(0).actualPos() << "  " 
-			<< controller->motionAtAbs(0).actualVel() << "  " 
-			<< controller->motionAtAbs(0).actualCur() << std::endl;
-	}
-	// log 目标位置、实际位置、实际速度、实际电流 //
-		
-	auto &lout = controller->lout();
-	lout << controller->motionAtAbs(0).targetPos() << "  " 
-		<< controller->motionAtAbs(0).actualPos() << "  " 
-		<< controller->motionAtAbs(0).actualVel() << "  " 
-		<< controller->motionAtAbs(0).actualCur() << "  ";
-	lout << std::endl;
-	// 返回total_count - target.count给aris实时核，值为-1，报错；值为0，结束；值大于0，继续执行下一个count
-	return total_count - target.count;
+    // 每1000ms打印 目标位置、实际位置//
+
+    if (target.count % 100 == 0)
+    {
+        for(aris::Size i=0; i<param.axis_pos.size();i++)
+        {
+            cout << controller->motionAtAbs(i).targetPos() << "  ";
+            cout << controller->motionAtAbs(i).actualPos() << "  ";
+        }
+        cout << std::endl;
+    }
+
+    cout << "total_count:" << total_count << std::endl;
+    // log //
+    auto &lout = controller->lout();
+    for(aris::Size i=0; i<param.axis_pos.size();i++)
+    {
+        lout << controller->motionAtAbs(i).targetPos() << "  ";
+        lout << controller->motionAtAbs(i).actualPos() << "  ";
+    }
+    lout << std::endl;
+    // 返回total_count - target.count给aris实时核，值为-1，报错；值为0，结束；值大于0，继续执行下一个count
+    return total_count - target.count;
 }
 auto MoveEAP::collectNrt(PlanTarget &target)->void {}
 MoveEAP::MoveEAP(const std::string &name) :Plan(name)
 {
-	command().loadXmlStr(
-		"<Command name=\"moveEAP\">"
-		"	<GroupParam>"
-		"		<Param name=\"begin_pos\" default=\"0.1\" abbreviation=\"b\"/>"
-		"		<Param name=\"pos\" default=\"0.1\"/>"
-		"		<Param name=\"vel\" default=\"0.02\"/>"
-		"		<Param name=\"acc\" default=\"0.3\"/>"
-		"		<Param name=\"dec\" default=\"-0.3\"/>"
-		"		<Param name=\"ab\" default=\"0\"/>"
-		"	</GroupParam>"
-		"</Command>");
+    command().loadXmlStr(
+        "<Command name=\"moveEAP\">"
+        "	<GroupParam>"
+        "		<Param name=\"begin_pos\" default=\"0.1\" abbreviation=\"b\"/>"
+        "		<Param name=\"pos\" default=\"0.1\"/>"
+        "		<Param name=\"vel\" default=\"0.5\"/>"
+        "		<Param name=\"acc\" default=\"1\"/>"
+        "		<Param name=\"dec\" default=\"1\"/>"
+        "		<Param name=\"ab\" default=\"0\"/>"
+        "	</GroupParam>"
+        "</Command>");
 }
 
 
@@ -301,7 +402,7 @@ auto createPlanRootRokaeXB4()->std::unique_ptr<aris::plan::PlanRoot>
 	auto &rs = plan_root->planPool().add<aris::plan::Reset>();
 	rs.command().findParam("pos")->setDefaultValue("{0.01}");
 	plan_root->planPool().add<MoveJS>();
-	plan_root->planPool().add<MoveEAP>();
+    plan_root->planPool().add<MoveEAP>();
 	return plan_root;
 }
 
@@ -317,11 +418,8 @@ int main(int argc, char *argv[])
 
 	//cs代表成员函数的引用，aris是头文件，server是命名空间，ControlServer是结构体
     auto&cs = aris::server::ControlServer::instance();
-    std::cout<<"1"<<std::endl;
     cs.resetController(createControllerRokaeXB4().release());
-    std::cout<<"2"<<std::endl;
     cs.resetPlanRoot(createPlanRootRokaeXB4().release());
-    std::cout<<"3"<<std::endl;
     std::cout<<"start controller server"<<std::endl;
 	//启动线程
 	cs.start();
